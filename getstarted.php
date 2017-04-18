70884e0a4e938163b5ef9751844863dae84f164c
Michael Rennie <Michael_Rennie@ca.ibm.com>
2017-04-17 22:14:03 -0500

    Bug 515271 - Remove crap from https://github.com/eclipse/orion

--- getstarted.php
+++ getstarted.php
@@ -1,8 +1,10 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
-<title>Getting Started Page Has Moved</title>
-<meta http-equiv="REFRESH" content="0;url=http://eclipse.org/orion"></HEAD>
-<BODY>
-</BODY>
-</HTML>
+   <meta http-equiv="refresh" content="0; url=https://orionhub.org">
+</head>
+<body>
+   <p>This page has moved to:
+   <a href="https://orionhub.org">https://orionhub.org</a></p>
+</body>
+</html>
